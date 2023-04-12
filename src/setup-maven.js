72d105f25a45c42adccb203b3a45e9ef66717445
Muhammad-Sarmad-khattak <sarmad.khattak@10pearls.com>
2023-04-12 21:11:52 +0500

    feat: added maven setup code
    jira: LPM-5444

---
+++ src/setup-maven.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const installer = require('./installer');
+
+async function run() {
+  try {
+    let version = '3.8.2';
+    if (version) {
+      await installer.getMaven(version);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
